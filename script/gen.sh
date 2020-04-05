bdd0f8d7af7d7e9b272fd39b9885d5ecb46d4c1f
Jeff Henrikson <jeff@groovescale.com>
2020-04-05 15:01:09 -0700

    implement: down.sh

--- script/gen.sh
+++ script/gen.sh
@@ -9,6 +9,7 @@ ${adirProj}/util/quote_heredoc.sh "${adirProj}/script/dockercompose.sh" "dockerc
 ${adirProj}/util/quote_heredoc.sh "${adirProj}/script/gen_self_sign.sh" "gen_self_sign.sh" EO4F
 ${adirProj}/util/quote_heredoc.sh "${adirProj}/script/installall.sh" "installall.sh" EO5F
 ${adirProj}/util/quote_heredoc.sh "${adirProj}/script/up.sh" "up.sh" EO6F
+${adirProj}/util/quote_heredoc.sh "${adirProj}/script/down.sh" "down.sh" EO6BF
 ${adirProj}/util/quote_heredoc.sh "${adirProj}/out/conf.json" "conf.json" EO7F
 ${adirProj}/util/quote_heredoc.sh "${adirProj}/util/install_reqs_and_run.sh" "install_reqs_and_run.sh" EO8F
 
